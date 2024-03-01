6008ec76d9dde5f1203b43180b661dd1db318dc3
dodokyo <ypd03008@naver.com>
2024-03-02 00:08:17 +0900

    ch5-1

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,3 +7,9 @@ export function cn(...inputs: ClassValue[]) {
 
 export const sleep = (ms: number) => new Promise((res) => setTimeout(res, ms));
 // await sleep(1000)
+
+export function getRandomInt(min: number, max: number) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
