ba13f789145c7c0d7b36f0f57694542eaf26092c
dodokyo <ypd03008@naver.com>
2024-03-02 13:22:38 +0900

    ch5-3

---
+++ components/elements/IconButton.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const IconButton = ({ icon, onClickIcon = () => {} }) => {
+  return (
+    <div
+      onClick={onClickIcon}
+      className="flex justify-center items-center w-[36px] h-[36px]
+       hover:bg-[rgba(144,144,144,0.45)] rounded-full cursor-pointer "
+    >
+      {icon}
+    </div>
+  );
+};
+
+export default IconButton;
--- components/elements/Logo.jsx
+++ components/elements/Logo.jsx
@@ -1,7 +1,30 @@
+"use client";
+import Image from "next/image";
+import { useRouter } from "next/navigation";
 import React from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
+import IconButton from "./IconButton";
 
 const Logo = () => {
-  return <div>Logo</div>;
+  const { push } = useRouter();
+
+  const onClickLogo = () => {
+    push("/"); // home 이동 하는 로직
+  };
+
+  const onClickMenu = () => {};
+
+  return (
+    <section className="flex flex-row items-center gap-3 ">
+      <IconButton
+        onClickIcon={onClickMenu}
+        icon={<RxHamburgerMenu size={24} />}
+      />
+      <div className=" cursor-pointer" onClick={onClickLogo}>
+        <Image alt="logo" width={100} height={30} src={"/main-logo.svg"} />
+      </div>
+    </section>
+  );
 };
 
 export default Logo;
