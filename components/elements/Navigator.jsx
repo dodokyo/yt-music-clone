a3e814290b35e532cac584198a4dcd07a237df02
dodokyo <ypd03008@naver.com>
2024-03-02 13:49:28 +0900

    ch5-5

--- components/elements/Navigator.jsx
+++ components/elements/Navigator.jsx
@@ -57,10 +57,20 @@ const Navigator = () => {
         <div className="w-full h-[1px] bg-neutral-700"></div>
       </section>
       <section className="px-6">
-        <div className="hover:bg-neutral-600 cursor-pointer flex flex-row items-center bg-neutral-700 my-6 rounded-3xl p-2 font-[200] justify-center gap-2">
+        <div
+          className="hover:bg-neutral-700 cursor-pointer
+         flex flex-row items-center bg-neutral-800 my-6 rounded-3xl p-2 font-[200] justify-center gap-2"
+        >
           <FiPlus size={24}></FiPlus>
           <span>새 재생목록</span>
         </div>
+      </section>
+      <section>
+        <ul className="flex flex-col">
+          {dummyPlaylistArray.map((playlist) => {
+            return <PlayListNav playlist={playlist}></PlayListNav>;
+          })}
+        </ul>
       </section>
     </div>
   );
