2c997e8d29d11247c282d230947011adbf1d18cf
dodokyo <ypd03008@naver.com>
2024-03-03 19:49:41 +0900

    ch6-6

--- components/Header.jsx
+++ components/Header.jsx
@@ -2,6 +2,8 @@ import Image from "next/image";
 import React from "react";
 import UserIcon from "@/components/UserIcon";
 import PagePadding from "@/components/PagePadding";
+import { FaChromecast } from "react-icons/fa";
+import { FiSearch } from "react-icons/fi";
 
 const Header = ({ children }) => {
   return (
@@ -20,10 +22,28 @@ const Header = ({ children }) => {
         </div>
       </section>
       {/* searchSection */}
-      <section className="sticky">
+      <section className="sticky ">
         <PagePadding>
-          searchSection
-          <UserIcon />
+          <div className="h-[64px] flex flex-row justify-between items-center">
+            <article
+              className="h-[42px] min-w-[480px] flex flex-row items-center
+            bg-[rgba(0,0,0,0.14)] rounded-2xl px-[16px] gap-[16px]
+            "
+            >
+              <div>
+                <FiSearch size={24} />
+              </div>
+              <input
+                className="h-full w-full bg-transparent "
+                placeholder="노래, 앨범, 아티스트, 팟캐스트 검색"
+                type="text"
+              />
+            </article>
+            <article className="flex flex-row gap-6 items-center">
+              <FaChromecast size={26} />
+              <UserIcon />
+            </article>
+          </div>
         </PagePadding>
       </section>
       <section className="relative">{children}</section>
