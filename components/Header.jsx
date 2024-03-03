c4ca7a1655092a05b805bb6bb945da211ff67aed
dodokyo <ypd03008@naver.com>
2024-03-03 19:19:15 +0900

    ch6-4

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import React from "react";
 import UserIcon from "@/components/UserIcon";
+import PagePadding from "@/components/PagePadding";
 
 const Header = ({ children }) => {
   return (
@@ -18,10 +19,14 @@ const Header = ({ children }) => {
           <div className=" absolute h-[400px] top-0 bg-gradient-to-t from-black w-full"></div>
         </div>
       </section>
-      <section>
-        <UserIcon />
+      {/* searchSection */}
+      <section className="sticky">
+        <PagePadding>
+          searchSection
+          <UserIcon />
+        </PagePadding>
       </section>
-      <section className="absolute">{children}</section>
+      <section className="relative">{children}</section>
     </header>
   );
 };
