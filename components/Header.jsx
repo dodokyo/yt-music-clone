a360c1eb91d5a739843a535b8826a5d80e055069
dodokyo <ypd03008@naver.com>
2024-03-03 19:14:07 +0900

    ch6-3

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import React from "react";
+import UserIcon from "@/components/UserIcon";
 
 const Header = ({ children }) => {
   return (
@@ -16,6 +17,9 @@ const Header = ({ children }) => {
           <div className=" absolute h-[400px] top-0 bg-black opacity-40 w-full"></div>
           <div className=" absolute h-[400px] top-0 bg-gradient-to-t from-black w-full"></div>
         </div>
+      </section>
+      <section>
+        <UserIcon />
       </section>
       <section className="absolute">{children}</section>
     </header>
