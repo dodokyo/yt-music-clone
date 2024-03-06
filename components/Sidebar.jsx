711db90b1df4b255e10de184dc741a179ac12280
dodokyo <ypd03008@naver.com>
2024-03-06 20:20:48 +0900

    ch8-5

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -13,7 +13,7 @@ const Sidebar = ({ children }) => {
           <Navigator />
         </div>
       </nav>
-      <div className="flex-1">{children}</div>
+      <div className="w-full lg:w-[calc(100%-240px)] ">{children}</div>
     </div>
   );
 };
--- components/SongCard.tsx
+++ components/SongCard.tsx
@@ -1,12 +1,31 @@
 import { TopSong } from "@/types";
+import Image from "next/image";
 import React from "react";
+import { FaCircle } from "react-icons/fa";
+import { AiOutlineCaretDown } from "react-icons/ai";
+import { AiOutlineCaretUp } from "react-icons/ai";
 
 interface SongCardProps {
   song: TopSong;
 }
 
 const SongCard: React.FC<SongCardProps> = ({ song }) => {
-  return <div>{song.name}</div>;
+  return (
+    <article className="flex flex-row gap-4 h-[48px] w-[400px] ">
+      <div className=" w-[48px] h-[48px] relative">
+        <Image src={song.imageSrc} alt="img" fill className=" object-cover" />
+      </div>
+      <div className="flex flex-row items-center gap-4">
+        {song.rank === song.prevRank ? (
+          <FaCircle size={8} />
+        ) : song.rank > song.prevRank ? (
+          <AiOutlineCaretUp color="#3CA63F" />
+        ) : (
+          <AiOutlineCaretDown color="#FF0000" />
+        )}
+      </div>
+    </article>
+  );
 };
 
 export default SongCard;
