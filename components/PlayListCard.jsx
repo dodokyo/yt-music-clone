3e4931f5ec311e784fa4759bda38fe546b9a3145
dodokyo <ypd03008@naver.com>
2024-03-06 19:13:03 +0900

    ch7-6

---
+++ components/PlayListCard.jsx
@@ -0,0 +1,58 @@
+"use client";
+import Image from "next/image";
+import React from "react";
+import { getRandomElementFromArray } from "@/lib/utils";
+import { useRouter } from "next/navigation";
+import { MdMoreVert } from "react-icons/md";
+import { FiPlay } from "react-icons/fi";
+import IconButton from "./elements/IconButton";
+
+const PlayListCard = ({ playlist }) => {
+  const { push } = useRouter();
+  const { id, owner, playlistName, songList } = playlist;
+
+  const songListLen = songList?.length;
+  const imageSrc = getRandomElementFromArray(songList).imageSrc;
+
+  const onClickCard = () => {
+    push(`/playlist?list=${id}`);
+  };
+
+  const onClickPlay = () => {
+    // TODO play
+  };
+
+  return (
+    <article className=" h-[240px] cursor-pointer group">
+      <section onClick={onClickCard} className=" relative h-[136px] ">
+        <Image
+          src={imageSrc}
+          fill={true}
+          alt="thumbnail"
+          className="object-cover"
+        />
+        <div className="hidden relative group-hover:block bg-gradient-to-b from-[rgba(0,0,0,0.7)] top-0 w-full h-[136px] ">
+          <div className=" absolute top-2 right-4">
+            <IconButton icon={<MdMoreVert size={20} />} />
+          </div>
+          <div
+            onClick={onClickPlay}
+            className=" absolute bottom-4 right-4 flex items-center justify-center
+           transform-gpu transition-transform hover:scale-110 
+           bg-[rgba(0,0,0,0.7)] w-[45px] h-[45px] rounded-full
+           hover:bg-[rgba(0,0,0,0.9)] pl-[1.5px]
+           "
+          >
+            <FiPlay size={22} color="red" />
+          </div>
+        </div>
+      </section>
+      <section className="mt-2">
+        <div>{playlistName}</div>
+        <div className="text-neutral-500">{`${owner} - 트랙 ${songListLen}개`}</div>
+      </section>
+    </article>
+  );
+};
+
+export default PlayListCard;
--- components/PlayListCarousel.tsx
+++ components/PlayListCarousel.tsx
@@ -7,6 +7,7 @@ import {
   CarouselNext,
   CarouselPrevious,
 } from "@/components/ui/carousel";
+import PlayListCard from "./PlayListCard";
 
 interface PlayListCarouselProps {
   title: string;
@@ -46,11 +47,13 @@ const PlayListCarousel: React.FC<PlayListCarouselProps> = ({
           </div>
         </div>
         <CarouselContent>
-          {Array.from({ length: 5 }).map((_, index) => (
-            <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
-              <div className="p-1">Card</div>
-            </CarouselItem>
-          ))}
+          {playlistArray?.map((playlist, index) => {
+            return (
+              <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
+                <PlayListCard playlist={playlist} />
+              </CarouselItem>
+            );
+          })}
         </CarouselContent>
       </Carousel>
     </div>
