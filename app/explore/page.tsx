86486bc74fe906471cace20464003254999c0c02
dodokyo <ypd03008@naver.com>
2024-03-06 20:01:50 +0900

    ch8-3

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,11 +1,18 @@
 import PagePadding from "@/components/PagePadding";
 import React from "react";
 import Category from "./components/Category";
-import { getAllPlaylist } from "@/lib/dummyData";
+import { getAllPlaylist, getSongListTop10 } from "@/lib/dummyData";
 import PlayListCarousel from "@/components/PlayListCarousel";
+import SongListCarousel from "@/components/SongListCarousel";
 
 const page = async () => {
-  const playlistArray = await getAllPlaylist();
+  // const playlistArray = await getAllPlaylist();
+  // const songListTop10 = await getSongListTop10();
+
+  const [playlistArray, songListTop10] = await Promise.all([
+    getAllPlaylist(),
+    getSongListTop10(),
+  ]);
 
   return (
     <PagePadding>
@@ -13,6 +20,8 @@ const page = async () => {
       <Category />
       <div className="mt-20"></div>
       <PlayListCarousel playlistArray={playlistArray} title="새 앨범 및 싱글" />
+      <div className="mt-20"></div>
+      <SongListCarousel songListTop10={songListTop10} title="새 앨범 및 싱글" />
     </PagePadding>
   );
 };
---
+++ components/SongListCarousel.tsx
@@ -0,0 +1,66 @@
+import { TopSong } from "@/types";
+import React from "react";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
+import PlayListCard from "./PlayListCard";
+
+interface SongListCarouselProps {
+  title: string;
+  subTitle?: string;
+  Thumbnail?: React.ReactNode;
+  songListTop10?: TopSong[];
+}
+
+const SongListCarousel: React.FC<SongListCarouselProps> = ({
+  title,
+  subTitle,
+  Thumbnail,
+  songListTop10,
+}) => {
+  return (
+    <div className="w-full">
+      <Carousel>
+        <div className="flex flex-row justify-between items-end my-2">
+          <article className="flex flex-row gap-3">
+            {Thumbnail}
+            <div className="flex flex-col justify-center">
+              <div>
+                {subTitle && (
+                  <div className=" text-neutral-500">{subTitle}</div>
+                )}
+              </div>
+              <div className="text-[34px] font-bold leading-[34px] ">
+                {title}
+              </div>
+            </div>
+          </article>
+          <div className="relative left-[-45px]">
+            <div className="absolute bottom-[20px]">
+              <CarouselPrevious className="right-2" />
+              <CarouselNext className=" left-2" />
+            </div>
+          </div>
+        </div>
+        <CarouselContent className="mt-4">
+          {songListTop10?.map((playlist, index) => {
+            return (
+              <CarouselItem
+                key={index}
+                className="basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5"
+              >
+                <PlayListCard playlist={playlist} />
+              </CarouselItem>
+            );
+          })}
+        </CarouselContent>
+      </Carousel>
+    </div>
+  );
+};
+
+export default SongListCarousel;
