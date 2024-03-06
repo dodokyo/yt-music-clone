c79987ffc527c5d1d7948b3b6607044445c79f6c
dodokyo <ypd03008@naver.com>
2024-03-06 19:54:42 +0900

    ch8-2

--- app/explore/page.tsx
+++ app/explore/page.tsx
@@ -1,12 +1,18 @@
 import PagePadding from "@/components/PagePadding";
 import React from "react";
 import Category from "./components/Category";
+import { getAllPlaylist } from "@/lib/dummyData";
+import PlayListCarousel from "@/components/PlayListCarousel";
 
-const page = () => {
+const page = async () => {
+  const playlistArray = await getAllPlaylist();
+
   return (
     <PagePadding>
       <div className="mt-4"></div>
       <Category />
+      <div className="mt-20"></div>
+      <PlayListCarousel playlistArray={playlistArray} title="새 앨범 및 싱글" />
     </PagePadding>
   );
 };
--- components/PlayListCarousel.tsx
+++ components/PlayListCarousel.tsx
@@ -51,7 +51,7 @@ const PlayListCarousel: React.FC<PlayListCarouselProps> = ({
             return (
               <CarouselItem
                 key={index}
-                className="md:basis-1/2 lg:basis-1/3 xl:basis-1/5"
+                className="basis-1/2 md:basis-1/3 lg:basis-1/4 xl:basis-1/5"
               >
                 <PlayListCard playlist={playlist} />
               </CarouselItem>
