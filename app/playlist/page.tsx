af68185e8cbcc9faca77c48252ee766df14c676d
dodokyo <ypd03008@naver.com>
2024-03-08 18:27:48 +0900

    ch10-3

--- app/playlist/page.tsx
+++ app/playlist/page.tsx
@@ -3,7 +3,8 @@ import { getRandomElementFromArray } from "@/lib/utils";
 import { permanentRedirect } from "next/navigation";
 import React from "react";
 import HeaderBgChanger from "@/components/HeaderBgChanger";
-
+import PagePadding from "@/components/PagePadding";
+import PlayListHead from "@/components/PlayListHead";
 interface PlaylistPageProps {
   searchParams: {
     list: string;
@@ -18,10 +19,11 @@ const page = async (props: PlaylistPageProps) => {
   const imageSrc = getRandomElementFromArray(playlist.songList)?.imageSrc;
 
   return (
-    <div>
+    <PagePadding>
       <HeaderBgChanger imageSrc={imageSrc} />
-      playlist {props.searchParams.list}{" "}
-    </div>
+      <div className="mt-12"></div>
+      <PlayListHead playlist={playlist} />
+    </PagePadding>
   );
 };
 
---
+++ components/PlayListHead.jsx
@@ -0,0 +1,36 @@
+"use client";
+import React from "react";
+import IconButton from "./elements/IconButton";
+import { FiMoreVertical, FiPlay } from "react-icons/fi";
+import Image from "next/image";
+import { getRandomElementFromArray } from "@/lib/utils";
+import DarkButton from "@/components/elements/DarkButton";
+import WhiteButton from "@/components/elements/WhiteButton";
+
+const PlayListHead = ({ playlist = {} } = {}) => {
+  const { playlistName, owner, songList } = playlist;
+
+  const randomSong = getRandomElementFromArray(songList);
+
+  return (
+    <section className="flex flex-col items-center gap-[50px] lg:flex-row">
+      <div className=" relative h-[240px] w-[240px]">
+        <Image alt="songImg" fill src={randomSong?.imageSrc} />
+      </div>
+      <article className="flex flex-col justify-center">
+        <div className="font-bold text-[28px]">{playlistName}</div>
+        <div className=" text-neutral-400 mt-4 text-[14px]">
+          <div>{`앨범 • ${owner} • 2019`}</div>
+          <div>{`${songList.length}곡 • 15분`}</div>
+        </div>
+        <ul>
+          <IconButton icon={<FiMoreVertical size={24} />} />
+          <WhiteButton icon={<FiPlay />} label="재생" onClick={() => {}} />
+          <DarkButton />
+        </ul>
+      </article>
+    </section>
+  );
+};
+
+export default PlayListHead;
