5f66a5ed5486b8dfe62f0295a0eea0da3aa8f000
dodokyo <ypd03008@naver.com>
2024-03-08 20:49:26 +0900

    ch11-2

--- app/channel/[id]/page.tsx
+++ app/channel/[id]/page.tsx
@@ -1,3 +1,8 @@
+import HeaderBgChanger from "@/components/HeaderBgChanger";
+import PagePadding from "@/components/PagePadding";
+import { getChannelById } from "@/lib/dummyData";
+import { getRandomElementFromArray } from "@/lib/utils";
+import { permanentRedirect } from "next/navigation";
 import React from "react";
 
 interface ChannelPageProps {
@@ -6,8 +11,19 @@ interface ChannelPageProps {
   };
 }
 
-const page = (props: ChannelPageProps) => {
-  return <div>channel/[{props.params.id}]</div>;
+const page = async (props: ChannelPageProps) => {
+  const channel = await getChannelById(Number(props.params.id));
+
+  if (!channel) permanentRedirect("/");
+
+  const imageSrc = getRandomElementFromArray(channel.songList)?.imageSrc;
+
+  return (
+    <PagePadding>
+      <HeaderBgChanger imageSrc={imageSrc} />
+      channel/[{props.params.id}]
+    </PagePadding>
+  );
 };
 
 export default page;
