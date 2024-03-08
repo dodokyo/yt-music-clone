ca219e4eeaa85eaf51a28203b34d3764da78bb4c
dodokyo <ypd03008@naver.com>
2024-03-08 21:05:23 +0900

    ch11-3

--- app/channel/[id]/page.tsx
+++ app/channel/[id]/page.tsx
@@ -1,9 +1,12 @@
 import HeaderBgChanger from "@/components/HeaderBgChanger";
 import PagePadding from "@/components/PagePadding";
+import DarkButton from "@/components/elements/DarkButton";
+import WhiteButton from "@/components/elements/WhiteButton";
 import { getChannelById } from "@/lib/dummyData";
 import { getRandomElementFromArray } from "@/lib/utils";
 import { permanentRedirect } from "next/navigation";
 import React from "react";
+import { FiMusic, FiShuffle } from "react-icons/fi";
 
 interface ChannelPageProps {
   params: {
@@ -21,7 +24,38 @@ const page = async (props: ChannelPageProps) => {
   return (
     <PagePadding>
       <HeaderBgChanger imageSrc={imageSrc} />
-      channel/[{props.params.id}]
+      <div className="mt-[150px]"></div>
+      <section>
+        <div className=" text-[28px] font-bold">{channel.name}</div>
+        <article className="mt-4 lg:hidden">
+          <div>
+            <DarkButton
+              className={"w-[230px] flex justify-center"}
+              label={"구독중 4.18만"}
+            />
+          </div>
+          <div className="flex flex-row gap-4 mt-4">
+            <WhiteButton
+              label={"셔플"}
+              icon={<FiShuffle size={16}></FiShuffle>}
+            />
+            <WhiteButton label={"뮤직 스테이션"} icon={<FiMusic size={16} />} />
+          </div>
+        </article>
+        <div className="hidden lg:flex flex-row items-center gap-4 text-[14px] mt-4">
+          <WhiteButton
+            label={"셔플"}
+            icon={<FiShuffle size={16}></FiShuffle>}
+          />
+          <WhiteButton label={"뮤직 스테이션"} icon={<FiMusic size={16} />} />
+          <DarkButton
+            className={"w-[230px] flex justify-center"}
+            label={"구독중 4.18만"}
+          />
+        </div>
+      </section>
+      <section>노래</section>
+      <section>앨범</section>
     </PagePadding>
   );
 };
