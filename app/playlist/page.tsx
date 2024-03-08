5a05032664530a0a6d7c6edc393df716d6cb9171
dodokyo <ypd03008@naver.com>
2024-03-08 19:36:50 +0900

    ch10-5,6

--- app/playlist/page.tsx
+++ app/playlist/page.tsx
@@ -5,6 +5,7 @@ import React from "react";
 import HeaderBgChanger from "@/components/HeaderBgChanger";
 import PagePadding from "@/components/PagePadding";
 import PlayListHead from "@/components/PlayListHead";
+import SongCardRowExpand from "@/components/SongCardRowExpand";
 interface PlaylistPageProps {
   searchParams: {
     list: string;
@@ -23,6 +24,12 @@ const page = async (props: PlaylistPageProps) => {
       <HeaderBgChanger imageSrc={imageSrc} />
       <div className="mt-12"></div>
       <PlayListHead playlist={playlist} />
+      <div className="mt-12"></div>
+      <section className="flex flex-col gap-2">
+        {playlist.songList.map((song, idx) => {
+          return <SongCardRowExpand song={song} key={idx} />;
+        })}
+      </section>
     </PagePadding>
   );
 };
---
+++ components/SongCardRowExpand.tsx
@@ -0,0 +1,57 @@
+"use client";
+import { Song } from "@/types";
+import Image from "next/image";
+import React from "react";
+import { FiPlayCircle } from "react-icons/fi";
+import { FiThumbsDown } from "react-icons/fi";
+import { FiThumbsUp } from "react-icons/fi";
+import { FiMoreVertical } from "react-icons/fi";
+import IconButton from "./elements/IconButton";
+import { useRouter } from "next/navigation";
+
+interface SongCardRowExpandProps {
+  song: Song;
+}
+
+const SongCardRowExpand: React.FC<SongCardRowExpandProps> = ({ song }) => {
+  const { channel, channelId } = song;
+  const { push } = useRouter();
+
+  const onClickChannel = () => {
+    push(`/channel/${channelId}`);
+  };
+
+  return (
+    <article
+      className="flex flex-row items-center gap-4 h-[48px] w-full
+        relative group"
+    >
+      <div className=" w-[48px] h-[48px] relative">
+        <Image src={song.imageSrc} alt="img" fill className=" object-cover" />
+        <section
+          className="hidden group-hover:flex absolute top-0 w-[48px] h-[48px] items-center justify-center bg-black
+        cursor-pointer
+        "
+        >
+          <FiPlayCircle size={20} />
+        </section>
+      </div>
+      <div className=" flex flex-row gap-4 justify-between basis-1/3">
+        <div className=" w-[130px] truncate">{song.name}</div>
+        <div
+          onClick={onClickChannel}
+          className=" text-neutral-500 hover:underline cursor-pointer"
+        >
+          {channel}
+        </div>
+      </div>
+      <section className="hidden group-hover:flex absolute top-0 right-0 flex-row justify-end items-center h-[48px] w-[120px] bg-[rgba(0,0,0,0.7)]">
+        <IconButton icon={<FiThumbsDown size={20} />} />
+        <IconButton icon={<FiThumbsUp size={20} />} />
+        <IconButton icon={<FiMoreVertical size={20} />} />
+      </section>
+    </article>
+  );
+};
+
+export default SongCardRowExpand;
