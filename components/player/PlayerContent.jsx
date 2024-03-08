9fbef90745a00f28e0c4d8ba40560dd9b62a970b
dodokyo <ypd03008@naver.com>
2024-03-08 22:51:13 +0900

    ch12-7

--- components/player/PlayerContent.jsx
+++ components/player/PlayerContent.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 import { Slider as PlayerSlider } from "@/components/ui/playerSlider";
+import { useAudio } from "react-use";
 
 const PlayerContent = () => {
+  const [audio, state, controls, ref] = useAudio({
+    src: null, //"/music/50meru - Canopus.mp4",
+    autoPlay: true,
+  });
+
   return (
     <div className="h-full w-full relative">
       <div className=" absolute top-[-16px] w-full">
         <PlayerSlider />
       </div>
+      {audio}
     </div>
   );
 };
