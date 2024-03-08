c18cae80a79254a80e94c9268600c8409bcf733b
dodokyo <ypd03008@naver.com>
2024-03-08 22:46:51 +0900

    ch12-6

--- hooks/usePlayerState.ts
+++ hooks/usePlayerState.ts
@@ -1,13 +1,67 @@
+import { Song } from "@/types";
 import { create } from "zustand";
 
 interface PlayerState {
   isVisiblePlayer: boolean;
   setIsVisiblePlayer: (isVisiblePlayer: boolean) => void;
+  activeSong?: Song | null;
+  prevPlayerQueue: Song[];
+  nextPlayerQueue: Song[];
+  // 기능들 (재생,다음곡,이전곡)
+  addSongList: (songList: Song[]) => void;
+  playNext: () => void;
+  playBack: () => void;
 }
 
 const usePlayerState = create<PlayerState>((set) => ({
   isVisiblePlayer: true,
   setIsVisiblePlayer: (isVisiblePlayer: boolean) => set({ isVisiblePlayer }),
+  activeSong: null,
+  prevPlayerQueue: [],
+  nextPlayerQueue: [],
+  addSongList: (songList: Song[]) =>
+    set((prev) => {
+      const prevSong = prev.activeSong;
+      const cloneSongList = [...songList];
+      const currentSong = cloneSongList.splice(0, 1)?.[0];
+
+      return {
+        activeSong: currentSong,
+        prevPlayerQueue: prevSong
+          ? [prevSong, ...prev.prevPlayerQueue]
+          : prev.prevPlayerQueue,
+        nextPlayerQueue: [...cloneSongList],
+        isVisiblePlayer: true,
+      };
+    }),
+  playNext: () =>
+    set((prev) => {
+      const currentSong = prev.activeSong;
+      const nextSrc = prev.nextPlayerQueue.splice(0, 1)?.[0];
+
+      return {
+        activeSong: nextSrc,
+        nextPlayerQueue: prev.nextPlayerQueue,
+        prevPlayerQueue: [
+          ...(currentSong ? [currentSong] : []),
+          ...prev.prevPlayerQueue,
+        ],
+      };
+    }),
+  playBack: () =>
+    set((prev) => {
+      const currentSong = prev.activeSong;
+      const preSrc = prev.prevPlayerQueue.splice(0, 1)?.[0];
+
+      return {
+        activeSong: preSrc,
+        nextPlayerQueue: [
+          ...(currentSong ? [currentSong] : []),
+          ...prev.nextPlayerQueue,
+        ],
+        prevPlayerQueue: prev.prevPlayerQueue,
+      };
+    }),
 }));
 
 export default usePlayerState;
