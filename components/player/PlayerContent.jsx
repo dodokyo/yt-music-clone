7ff7528447d0aae718c049a5571ef6531a83117e
dodokyo <ypd03008@naver.com>
2024-03-08 23:25:30 +0900

    ch-9,10

--- components/player/PlayerContent.jsx
+++ components/player/PlayerContent.jsx
@@ -2,17 +2,24 @@
 import React from "react";
 import { Slider as PlayerSlider } from "@/components/ui/playerSlider";
 import { useAudio } from "react-use";
-import { IoPlaySkipBackSharp, IoPlaySkipForwardSharp } from "react-icons/io5";
-import { AiOutlinePause } from "react-icons/ai";
+import {
+  IoPlaySkipBackSharp,
+  IoPlaySkipForwardSharp,
+  IoShuffle,
+  IoVolumeHighOutline,
+} from "react-icons/io5";
+import { AiFillCaretUp, AiOutlinePause } from "react-icons/ai";
 import usePlayerState from "@/hooks/usePlayerState";
 import { ClipLoader } from "react-spinners";
 import { RiPlayFill } from "react-icons/ri";
+import Image from "next/image";
+import { RxLoop } from "react-icons/rx";
 
 const PlayerContent = () => {
   const { activeSong } = usePlayerState();
   const [audio, state, controls, ref] = useAudio({
     src: activeSong?.src, //"/music/50meru - Canopus.mp4",
-    autoPlay: true,
+    autoPlay: false,
   });
 
   const isLoading = activeSong?.src && state.buffered?.length === 0;
@@ -61,8 +68,34 @@ const PlayerContent = () => {
             onClick={onClickNextBtn}
           />
         </div>
-        <article></article>
-        <div></div>
+        <article>
+          <div className=" flex flex-row gap-4 items-center">
+            <div className=" relative w-[40px] h-[40px]">
+              <Image
+                fill
+                className="object-cover"
+                src={activeSong?.imageSrc}
+                alt="img"
+              ></Image>
+            </div>
+            <div className="flex flex-col">
+              <div>{activeSong?.name}</div>
+              <div className=" text-neutral-500 text-[14px]">
+                {activeSong?.channel} • 조회수 7.8만회 • 좋아요 1.7천개
+              </div>
+            </div>
+          </div>
+        </article>
+        <div className="flex flex-row gap-2">
+          <div className=" hidden lg:flex flex-row gap-2 ">
+            <IoVolumeHighOutline size={24} className=" cursor-pointer" />
+            <IoShuffle size={24} className=" cursor-pointer" />
+            <RxLoop size={24} className=" cursor-pointer" />
+          </div>
+          <div>
+            <AiFillCaretUp size={24} className=" cursor-pointer" />
+          </div>
+        </div>
       </section>
     </div>
   );
